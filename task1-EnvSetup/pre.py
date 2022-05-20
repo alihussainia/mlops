fa0874d414c8ac4c10dcae52abe4e67b05ea16a1
MUHAMMAD ALI <muhammad.ali@dpschool.io>
2022-05-20 17:51:01 +0500

    updated soft path from relative to absolute

--- task1-EnvSetup/pre.py
+++ task1-EnvSetup/pre.py
@@ -3,7 +3,7 @@ call("wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh",
 call("bash Anaconda3-2022.05-Linux-x86_64.sh", shell=True)
 call("sudo apt update", shell=True)
 call("sudo apt install docker.io", shell=True)
-call("mkdir soft", shell=True)
+call("mkdir ~/soft", shell=True)
 # call("cd /home/ubuntu/soft/", shell=True)
 call("wget https://github.com/docker/compose/releases/download/v2.5.1/docker-compose-linux-x86_64 -O ~/soft/docker-compose", shell=True)
 call("chmod +x ~/soft/docker-compose", shell=True)
@@ -23,4 +23,4 @@ call("sudo usermod -aG docker $USER", shell=True)
 # activate the changes
 call("newgrp docker", shell=True)
 # Log out and log back in so that your group membership is re-evaluated.
-call("exit", shell=True) 
+call("exit", shell=True)
