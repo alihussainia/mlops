9111ffc3f377e8bcc0deb03d4f8dd2f0abf44207
MUHAMMAD ALI <muhammad.ali@dpschool.io>
2022-05-24 11:01:51 +0500

    Updated the data url from 2022 to 2021

--- task1-EnvSetup/post.py
+++ task1-EnvSetup/post.py
@@ -7,6 +7,6 @@ call("git clone https://github.com/DataTalksClub/mlops-zoomcamp.git", shell=True
 call("mkdir -p ~/notebooks/data/", shell=True)
 # getting data
 # ref: https://www1.nyc.gov/site/tlc/about/tlc-trip-record-data.page
-call("wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2022-01.parquet -O ~/notebooks/data/jan.parquet", shell=True)
-# to get Feb 2022 data while above one is of Jan, 22.
-call("wget https://nyc-tlc.s3.amazonaws.com/trip+data/green_tripdata_2022-02.parquet -O ~/notebooks/data/feb.parquet", shell=True)
+call("wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2021-01.parquet -O ~/notebooks/data/jan.parquet", shell=True)
+# to get Feb 2021 data while above one is of Jan, 21 Green Taxi.
+call("wget https://s3.amazonaws.com/nyc-tlc/trip+data/green_tripdata_2021-02.parquet -O ~/notebooks/data/feb.parquet", shell=True)
